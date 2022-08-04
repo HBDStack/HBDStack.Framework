1e5f47e0427afd4ba083222ecbfa9918dce8cf22
Steven Hoang <hoangbaoduy@gmail.com>
2022-08-04 19:48:35 +0800

    remove encrypt

--- HBDStack.Framework.Extensions.Tests/StringEncryptionTests.cs
+++ HBDStack.Framework.Extensions.Tests/StringEncryptionTests.cs
@@ -9,22 +9,6 @@ namespace HBDStack.Framework.Extensions.Tests;
 [TestClass]
 public class StringEncryptionTests
 {
-    private const string Key = "8!DQQx@5.~#j_JM>ri.vAF2A.*q_.zYx";
-
-    [TestMethod]
-    public void EncryptWithPassword_Test()
-    {
-        var s = "Steven Hoang";
-        var e = s.EncryptWith(Key);
-        e.Should().NotBe(s);
-        e.IsEncrypted().Should().BeTrue();
-        
-        var a = () => e.DecryptWith("03d!C3h^Z#XH52Ek8E?sNFcEEAP2z.?}");
-        a.Should().Throw<CryptographicException>();
-
-        e.DecryptWith(Key).Should().Be(s);
-    }
-    
     [TestMethod]
     public void EncryptWithBase64_Test()
     {
